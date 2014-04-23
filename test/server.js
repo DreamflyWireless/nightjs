62b89f85e7da050018cdffefd1d2f822b852097f
kerry95 <95kerry@gmail.com>
2014-04-23 23:01:57 +0800

    Add test server

---
+++ test/server.js
@@ -0,0 +1,39 @@
+var port
+  , connect
+  , app
+  , http
+
+connect = require('connect')
+http = require('http')
+
+port = process.argv[2]
+if(!port) port = 4321
+else if(port === '-h' || port === '--help'){
+  printUsage()
+  process()
+}
+else{
+  // make sure `port` is interger
+  port = parseInt(port)
+  if(isNaN(port)){
+    printUsage()
+    process(1)
+  }
+}
+
+app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.static(__dirname + '/public'))
+
+http.createServer(app).listen(port)
+
+// helper functions
+function printUsage(){
+  var argv, spliter
+  argv = process.argv
+  spliter = process.platform === 'win32' ? '\\' : '/'
+  console.log(
+    'Usage: ' +
+    ['node', argv[1].split(spliter).pop(), '<port>'].join(' ')
+  )
+}
