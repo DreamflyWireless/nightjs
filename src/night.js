21027d274e3a9207138a6662cbd449ed43f7fe92
kerry95 <95kerry@gmail.com>
2014-04-27 23:16:33 +0800

    Rebuild night.js core

--- src/night.js
+++ src/night.js
@@ -1,79 +1,222 @@
-(function(window){
+var night = (function(window){
+  var undefined
+    // * Quicker access
+    // * Save native API used to be compatible with that
+    // subsequent codes may override native API
+    , document = window.document
+    , Array = window.Array
+    , arrProto = Array.prototype
+    , slice = arrProto.slice
+    , filter = arrProto.filter
+    , Object = window.Object
+    , objProto = Object.prototype
+    , objProtoToString = objProto.toString
+    , Node = window.Node
+    , ELEMENT_NODE = Node.ELEMENT_NODE || 1
+    , DOCUMENT_NODE = Node.DOCUMENT_NODE || 9
+    , READY_REG = /complete|loaded|interactive/
+    , NIGHT_INSTANCE_STR = '[object Night]'
 
-// TODO: check whether the same name global variable exist
-window.$ = window.simple = $;
+    // APIs on this namespace are exported for deeply
+    // customizing night.js
+    , night = {}
+    , $
 
-var document = window.document,
-    Array = window.Array,
-    Object = window.Object,
-    arraySlice = Array.prototype.slice;
-var Night = function(selector){
-    if(selector){
-        // abbr. elements
-        this._els = [];
+  // @example $()
+  //
+  // @example $(selector, context)
+  // @param selector {String} a CSS selector
+  // @param [context] {HTMLDocument|Element|night.N} context in
+  // which query elements
+  //
+  // @example $(nightCollection)
+  // @param nightCollection {night.N} night collection
+  //
+  // @example $(element)
+  // @param element {Element} DOM element
+  //
+  // @example $(nodeList)
+  // @param nodeList {NodeList|HTMLCollection} DOM node list
+  //
+  // @exapmle $(elementArray)
+  // @param elementArray {Array} array consist of DOM element
+  //
+  // @example $(callback)
+  // @param callback {Function} callback invoked when DOM is
+  // ready
+  //
+  // TODO:
+  // @example $(htmlFragment, ownerDocument)
+  // @param htmlFragment {String} a string of html to create
+  // @param [ownerDocument] {HTMLDocument}
+  //
+  // @example $(htmlFragment, attributes)
+  // @param htmlFragment
+  // @param [attributes]
+  //
+  // @return {night.N} a night collection
+  $ = function(selector, context){
+    var dom
+    // Still return an empty night collection to make
+    // subsequent processing work
+    if(!selector) return new night.N()
+
+    else if(typeof selector === 'string'){
+      selector = selector.trim()
+      dom = night.qsa(document, selector)
+
+      if( context ){
+        if( $.isHTMLDocument(context) &&
+          $.isElement(context) &&
+          $.isN(context) )
+          return $(context).find(selector)
+        else{
+          console.error('Second argument is invalide')
+          return new night.N()
+        }
+      }
+
+      else dom = night.qsa(document, selector)
     }
-    else{
-        this._els = arraySlice.call(
-            document.querySelectorAll(selector), 0
-        );
+
+    else if( $.isN(selector) )
+      return selector
+
+    else if( $.isElement(selector) )
+      dom = [selector], selector = null
+
+    // If first argument is an instance of `NodeList` or
+    // `HTMLCollection`, conver it to array
+    else if( $.isNodeList(selector) || $.isHTMLCollection(selector) )
+      selector = slice.call(selector)
+
+    // If first argument is array, filter out items that are
+    // not DOM element
+    else if( $.isArray(selector) )
+      dom = filter.call(selector, function(item){
+        return $.isElement(item)
+      })
+
+    else if( $.isFunction(selector) )
+      return $(document).ready(selector)
+
+    // Wrap as night collection from the array of nodes found
+    return new night.N(dom, selector)
+  }
+
+  // Constructor for night collection
+  // @param elementArray {Array|undefined} array consist of
+  // DOM elements
+  // @param [selector] {String} selector for `dom`
+  // @return {night.N}
+  night.N = function(elementArray, selector){
+    var nightCollection = elementArray || []
+    // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
+    // Make element array to be night collection by using
+    // night collection's prototype as element array's
+    // prototype
+    Object.setPrototypeOf(nightCollection, $.fn)
+    nightCollection.selector = selector || ''
+    return nightCollection
+  }
+
+  // Night collection's prototype.
+  // Use `$.fn` as quick accessing and public API for
+  // `night.N.prototype`
+  $.fn = night.N.prototype
+  // The prototype of night collection's prototype is array's
+  // prototype, in other words, night collection inherit
+  // `$.fn`, and `$.fn` inherit `Array`
+  Object.setPrototypeOf($.fn, arrProto)
+
+  // Follow the best practice from _JavaScript: the Definitive
+  // Guide_ 6th edition: Core JavaScript Reference ->
+  // Object.toString()
+  $.fn.toString = function(){
+    return NIGHT_INSTANCE_STR
+  }
+
+  $.fn.get = function(index){
+    return index === undefined ?
+      // Without a parameter, `.get()` returns an array of
+      // all of the elements, but not night collection
+      slice.call(this) :
+      this[index >= 0 ? index : index + this.length]
+  }
+
+  // TODO
+  $.fn.on = function(){}
+
+  $.fn.ready = function(callback){
+    // `regExp.test()` is faster than `switch`
+    // Ref: http://jsperf.com/regexp-test-vs-switch
+    if( READY_REG.test(document.readyState) ) callback($)
+    else document.addEventListener(
+      'DOMContentLoaded',
+      function(){
+        callback($)
+      },
+      false
+    )
+    return this
+  }
+
+  // Start: static method on `$`
+  ;['Arguments', 'Function', 'String', 'Number', 'Date',
+  'RegExp', 'NodeList', 'HTMLCollection', 'HTMLDocument']
+  .forEach(function(name){
+    this['is' + name] = function(obj){
+      return objProtoToString.call(obj) === '[object ' + name + ']'
     }
-};
-Night.prototype = {
-    constructor: Night,
-    get: function(index){
-        return this._els[index];
-    },
-    // TODO: delegate event
-    on: function(eventName, handler){
-        this._els.forEach(function(el){
-            el.addEventListener(eventName, handler, false);
-        });
-        return this;
+  }, $)
+
+  $.isArray = Array.isArray
+
+  $.isElement = function(obj){
+    return !!(obj && obj.nodeType === ELEMENT_NODE)
+  }
+
+  // Treat object as what is not primitive
+  $.isObject = function(obj){
+    return obj === Object(obj)
+  }
+  // End: static method on `$`
+
+  // TODO: optimize for special cases, eg. `#id`, `.class`
+  night.qsa = function(element, selector){
+    switch(element.nodeType){
+      case ELEMENT_NODE:
+      case DOCUMENT_NODE:
+        return slice.call( element.querySelectorAll(selector) )
+      default:
+        return []
     }
-};
+  }
 
-// TODO: unit test
-$.bind = function(func, context){
-    var extraArgs = [],
-        startIndex;
+  night.isN = function(obj){
+    return obj.toString() === NIGHT_INSTANCE_STR
+  }
 
-    if(context){
-        startIndex = 2;
-    }
-    else{
-        context = window;
-        startIndex = 1;
-    }
+  night.find = function(selector){
+    var result
+    // Optimize certain case
+    if(this.length === 1)
+      result = $( night.qsa(this[0], selector) )
+    else
+      result = this.map(function(){
+        return night.qsa(this, selector)
+      })
+    return result
+  }
 
-    if(arguments.length > startIndex){
-        extraArgs = arraySlice.call(arguments, startIndex);
-    }
+  // Export internal API in the `$.night` namespace to enable
+  // deeply customizing night.js as users of night.js want
+  $.night = night
 
-    return function(){
-        var args = arraySlice.call(arguments);
-        args = args.concate(extraArgs);
-        func.apply(context, args);
-    };
-};
+  return $
+})(window)
 
-// TODO: unit test
-$.isElement = function(obj){
-    return obj &&
-        (obj instanceof HTMLElement ||
-        obj.nodeType === Node.ELEMENT_NODE);
-};
-
-// @example $(selector)
-// @example $(elements)
-function $(selector){
-    if(!selector) return [];
-    if(typeof selector === 'string'){
-        selector = selector.trim();
-        return new Night(selector);
-    }
-    else if(arguments){
-
-    }
-}
-
-})(this);
+// Make sure exporting `night` to global
+window.night = night
+// Alias `$` if `$` is not yet defined in global
+window.$ === undefined && (window.$ = night)
